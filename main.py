d46fa7d8cfe4fe49c2289ffcf2e9d6f4c7c72d0d
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2024-07-25 16:33:43 +0100

    redo with using numbers

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ third_digit = (user_number % 1000) // 100
 fourth_digit = (user_number % 100) // 10
 fifth_digit = user_number % 10
 
-user_reversed_number = int(str(fifth_digit) + str(fourth_digit) + str(third_digit) + str(second_digit) + str(first_digit))
-
-print("Your number: " + str(user_number))
-print("Your reversed number: " + str(user_reversed_number))
+# user_reversed_number = int(str(fifth_digit) + str(fourth_digit) + str(third_digit) + str(second_digit) + str(first_digit))
+user_reversed_number = (
+    fifth_digit * 10000 +
+    fourth_digit * 1000 +
+    third_digit * 100 +
+    second_digit * 10 +
+    first_digit
+)
+print("Your number: ", user_number)
+print("Your reversed number: ", user_reversed_number)
 
